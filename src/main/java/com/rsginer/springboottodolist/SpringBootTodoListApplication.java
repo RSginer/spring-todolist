d61297bf7c74d078aab8667a0d995a14ea01172f
Ruben Soler <ruben.soler@mimacom.com>
2021-05-07 21:04:02 +0200

    First commit

---
+++ src/main/java/com/rsginer/springboottodolist/SpringBootTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.rsginer.springboottodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTodoListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTodoListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsginer/springboottodolist/SpringBootTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsginer.springboottodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
