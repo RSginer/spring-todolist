d4e860fe197f6f3b44c7d63b1c7093f57969748c
Ruben Soler <ruben.soler@mimacom.com>
2021-05-08 14:43:57 +0200

    Feature add Swagger

--- src/main/java/com/rsginer/springboottodolist/config/AppConfig.java
+++ src/main/java/com/rsginer/springboottodolist/config/AppConfig.java
@@ -6,9 +6,24 @@ import com.rsginer.springboottodolist.user.service.AppUserService;
 import com.rsginer.springboottodolist.user.service.AppUserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class AppConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
     @Bean
     public TaskService taskService() {
         return new TaskServiceImpl();
