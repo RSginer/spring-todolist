d89e42e1090a2c5a8e0119b44c828c3d5897b046
Ruben Soler <ruben.soler@mimacom.com>
2021-05-10 19:06:09 +0200

    Lint imports

--- src/test/java/com/rsginer/springboottodolist/security/WithMockAppUser.java
+++ src/test/java/com/rsginer/springboottodolist/security/WithMockAppUser.java
@@ -1,7 +1,6 @@
 package com.rsginer.springboottodolist.security;
 
 import org.springframework.security.test.context.support.WithSecurityContext;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
--- src/test/java/com/rsginer/springboottodolist/task/api/TaskRESTControllerTest.java
+++ src/test/java/com/rsginer/springboottodolist/task/api/TaskRESTControllerTest.java
@@ -1,19 +1,11 @@
 package com.rsginer.springboottodolist.task.api;
 
 import com.rsginer.springboottodolist.security.WithMockAppUser;
-import com.rsginer.springboottodolist.security.auth.UserDetailsServiceImpl;
-import com.rsginer.springboottodolist.task.service.TaskService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
