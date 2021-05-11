0b04ac02ed0578ee1d67c15941ada6ef517ddae7
Ruben Soler <ruben.soler@mimacom.com>
2021-05-11 19:29:46 +0200

    Typo Task tag

--- src/main/java/com/rsginer/springboottodolist/task/api/TaskRESTController.java
+++ src/main/java/com/rsginer/springboottodolist/task/api/TaskRESTController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@Tag(name = "Tasks")
+@Tag(name = "Task")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskRESTController {
