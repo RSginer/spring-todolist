38980550ed955ff8076e85196ab6105340e4ec32
Ruben Soler <ruben.soler@mimacom.com>
2021-05-11 12:46:26 +0200

    Test signUp endpoint

--- src/test/java/com/rsginer/springboottodolist/SpringBootTodoListApplicationTests.java
+++ src/test/java/com/rsginer/springboottodolist/SpringBootTodoListApplicationTests.java
@@ -4,8 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.rsginer.springboottodolist.task.api.TaskRESTController;
 import com.rsginer.springboottodolist.task.service.TaskService;
+import com.rsginer.springboottodolist.user.api.AppUserRESTController;
 import com.rsginer.springboottodolist.user.service.AppUserService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -21,11 +22,15 @@ class SpringBootTodoListApplicationTests {
     @Autowired
     private TaskRESTController taskRESTController;
 
+    @Autowired
+    private AppUserRESTController appUserRESTController;
+
     @Test
     void contextLoads() {
         assertThat(taskService).isNotNull();
         assertThat(appUserService).isNotNull();
         assertThat(taskRESTController).isNotNull();
+        assertThat(appUserRESTController).isNotNull();
     }
 
 }
--- src/test/java/com/rsginer/springboottodolist/user/api/AppUserRESTControllerTest.java
+++ src/test/java/com/rsginer/springboottodolist/user/api/AppUserRESTControllerTest.java
@@ -1,11 +1,24 @@
 package com.rsginer.springboottodolist.user.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rsginer.springboottodolist.user.domain.AppUser;
+import com.rsginer.springboottodolist.user.service.AppUserService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AppUserRESTController.class)
@@ -13,5 +26,34 @@ import org.springframework.test.web.servlet.MockMvc;
 public class AppUserRESTControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    private AppUserService appUserService;
+
+    @Test
+    public void shouldCreateNewUser() throws Exception {
+        var user = new AppUser();
+        user.setUsername("test@test.com");
+        user.setFirstName("Test");
+        user.setLastName("Test");
+        user.setPassword("Test");
+
+        when(appUserService.signUp(any(AppUser.class))).thenReturn(user);
+
+        this.mockMvc.perform(
+                post("/api/user/signUp")
+                        .content(objectMapper.writeValueAsString(user))
+                        .contentType(MediaType.APPLICATION_JSON)
+        )
+                .andDo(print())
+                .andExpect(status()
+                        .isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value(user.getUsername()));
+    }
+
 
 }
