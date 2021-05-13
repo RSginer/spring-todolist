582131d38d261d00fde6117a074149721d008072
Ruben Soler <ruben.soler@mimacom.com>
2021-05-13 12:48:17 +0200

    Lint remove unused imports

--- src/main/java/com/rsginer/springboottodolist/task/domain/Task.java
+++ src/main/java/com/rsginer/springboottodolist/task/domain/Task.java
@@ -10,8 +10,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
